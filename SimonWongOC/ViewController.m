0b30022414dd14627612613e2a9f45d91fe167e7
SimonWong <codersimonwong@gmail.com>
2020-08-23 16:17:27 +0800

    1

--- SimonWongOC/ViewController.m
+++ SimonWongOC/ViewController.m
@@ -11,6 +11,8 @@
 
 @interface ViewController ()
 
+//@property (nonatomic, strong) void(^block)(void);
+
 @end
 
 @implementation ViewController
@@ -19,8 +21,28 @@
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor randomColor];
     
+//    __block int a = 1;
+//    self.block = ^{
+//        a++;
+//        NSLog(@"CurrentFunction:%s", __func__);
+//    };
+//
+//    NSLog(@"block:%@", self.block);
+    
+    int a = 1;
+    void(^block)(void) = ^() {
+        NSLog(@"a:%d", a);
+        NSLog(@"CurrentFunction:%s", __func__);
+    };
+    
+    NSLog(@"block:%@", block);
+    
+}
 
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    
 
+    
 }
 
 @end
